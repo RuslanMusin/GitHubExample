ccf02adbd52709fd17d3d9b1be7edb2f7edaf000
Ruslan Musin <kavin91@mail.ru>
2017-08-30 19:19:56 +0300

    Second commit

---
+++ GitHubExample.java
@@ -0,0 +1,11 @@
+package GitHubExample;
+
+/**
+ * Created by user on 30.08.2017.
+ */
+public class GitHubExample {
+
+    public static void main(String [] args){
+        System.out.println("Do something wrong or right!");
+    }
+}
