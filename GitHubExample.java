ba1f153238fb156a19f0b47a70e11d88b6b9770b
Ruslan Musin <kavin91@mail.ru>
2017-08-31 18:56:47 +0300

    merge

--- GitHubExample.java
+++ GitHubExample.java
@@ -6,7 +6,7 @@ package GitHubExample;
 public class GitHubExample {
 
     public static void main(String [] args){
-        System.out.println("Do something wrong or right!");
+        System.out.println("Do agasgasgssomething wrong or right!");
         System.out.println("Do something right");
     }
 }
