9f3caa421468509cbc535718caa1e3c8dcc8bbc0
Ruslan Musin <kavin91@mail.ru>
2017-08-31 19:01:19 +0300

    merge3

--- GitHubExample.java
+++ GitHubExample.java
@@ -10,6 +10,6 @@ public class GitHubExample {
     public static void main(String [] args){
         System.out.println("Do agasgasgssomething wrong or right!");
         System.out.println("Do something right");
-        System.out.println("Something really wrong");
+        Arrays.asList("different things");
     }
 }
