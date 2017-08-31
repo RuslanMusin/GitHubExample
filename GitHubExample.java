e1b0b8cbc5d9fe26a983e3970f976162b9bebcca
Ruslan Musin <kavin91@mail.ru>
2017-08-31 19:00:00 +0300

    merge2

--- GitHubExample.java
+++ GitHubExample.java
@@ -1,5 +1,7 @@
 package GitHubExample;
 
+import java.util.Arrays;
+
 /**
  * Created by user on 30.08.2017.
  */
@@ -8,5 +10,6 @@ public class GitHubExample {
     public static void main(String [] args){
         System.out.println("Do agasgasgssomething wrong or right!");
         System.out.println("Do something right");
+        Arrays.asList("different things");
     }
 }
